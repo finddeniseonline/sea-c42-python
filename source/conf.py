2d7c87a90fb06ddd2c88366443d4473565a0212a
Paul Pham <ppham@local-box.org>
2015-03-16 18:14:39 -0700

    Add changes for Session 1.

--- source/conf.py
+++ source/conf.py
@@ -120,7 +120,7 @@ html_theme_path = ['_themes',]
 
 # The name for this set of Sphinx documents.  If None, it defaults to
 # "<project> v<release> documentation".
-html_title = 'Code Fellows F2 Python, November-December 2014'
+html_title = 'Code Fellows F2 Python, March-April 2015'
 
 # A shorter title for the navigation bar.  Default is the same as html_title.
 html_short_title = 'F2 Python'
